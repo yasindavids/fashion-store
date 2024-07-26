7ac3a1870d9e04271b1a754c46f6b9a39e712892
Yasin Davids <135144054+yasindavids@users.noreply.github.com>
2024-07-26 13:49:17 +0200

    initial commit

---
+++ src/app/men/page.tsx
@@ -0,0 +1,8 @@
+export default function Men() {
+  return (
+    <div className="main-content">
+      <h1>Men Page</h1>
+      <p>This is the men page.</p>
+    </div>
+  );
+}
---
+++ src/app/women/page.tsx
@@ -0,0 +1,8 @@
+export default function Women() {
+  return (
+    <div className="main-content">
+      <h1>Women Page</h1>
+      <p>This is the women page.</p>
+    </div>
+  );
+}
